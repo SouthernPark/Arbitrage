11f1db058fb12a93aa256fd05582e459cf358e23
SouthernPark <2848919007@qq.com>
2021-11-07 18:09:43 -0500

    initial

---
+++ project3.py
@@ -0,0 +1,40 @@
+"""
+Math 560
+Project 3
+Fall 2021
+
+Partner 1:
+Partner 2:
+Date:
+"""
+
+# Import math and p3tests.
+import math
+from p3tests import *
+
+################################################################################
+
+"""
+detectArbitrage
+"""
+def detectArbitrage(adjList, adjMat, tol=1e-15):
+    ##### Your implementation goes here. #####
+    return []
+    ##### Your implementation goes here. #####
+
+################################################################################
+
+"""
+rates2mat
+"""
+def rates2mat(rates):
+    ##### Your implementation goes here. #####
+    # Currently this only returns a copy of the rates matrix.
+    return [[R for R in row] for row in rates]
+    ##### Your implementation goes here. #####
+
+"""
+Main function.
+"""
+if __name__ == "__main__":
+    testRates()
